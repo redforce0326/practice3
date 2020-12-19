848b9a16cffb5ebadd6462549678a9dbc9649ef1
EC2 Default User <ec2-user@ip-172-31-63-12.ec2.internal>
2020-12-19 12:27:49 +0000

    retry

---
+++ lesson3.js
@@ -0,0 +1,8 @@
+/* global $*/
+ $('.slider-contents').slick({
+   arrows: true,
+   prevArrow:'<div class="prev"></div>',
+	 nextArrow:'<div class="next"></div>',
+	 autoplay: true,
+	 autoplaySpeed: 4000,
+ });
